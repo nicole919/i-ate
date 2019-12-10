e61bc0eff938250931397f57c1b7912c1b439cc0
Nicole Fafard <nicole.fafard@gmail.com>
2019-12-10 15:30:10 -0700

    working add entry endpoints

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import RegistrationForm from "../RegistrationForm/RegistrationForm";
 import LoginForm from "../LoginForm/LoginForm";
 import Nav from "../Nav/Nav";
-import AteListItem from "../AteListItem/AteListItem";
+import AteList from "../AteList/AteList";
 import NewItemAteForm from "../NewAteItemForm/NewAteItemForm";
 import HomePage from "../HomePage/HomePage";
 
@@ -20,7 +20,7 @@ class App extends Component {
             <Route exact path="/" component={HomePage} />
             <Route path="/registrationform" component={RegistrationForm} />
             <Route path="/loginform" component={LoginForm} />
-            <Route path="/AteListItem" component={AteListItem} />
+            <Route path="/AteList" component={AteList} />
             <Route path="/NewAteItemForm" component={NewItemAteForm} />
           </Switch>
         </main>
